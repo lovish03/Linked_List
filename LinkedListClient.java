058ca55eecbb0d4c954f8b227346db913bd58264
Lovish <lovish@hemants-MacBook-Pro.local>
2016-09-05 14:25:33 +0530

    Linked List with Custom Iterator

--- LinkedListClient.java
+++ LinkedListClient.java
@@ -1,8 +1,17 @@
 import java.util.*;
 public class LinkedListClient 
 {
+  public void iterate(LinkedList linkedList)
+  {
+    for(Iterator itr=linkedList.iterator();itr.hasNext();)
+           {
+            System.out.println(itr.next());
+           }
+  }
+
     public static void main(String[] args) 
     {
+           LinkedListClient linkedListClient=new LinkedListClient();
            LinkedList<Integer> linkedList = new LinkedList<Integer>(); 
            System.out.println("LinkedList is");
            linkedList.insert(11);
@@ -10,21 +19,17 @@ public class LinkedListClient
            linkedList.insert(22);
            linkedList.insert(14);
            linkedList.insert(39);
-           System.out.println("Size of LinkedList is:"+linkedList.size());
+           linkedListClient.iterate(linkedList);
+          System.out.println("Size of LinkedList is:"+linkedList.size());
 
-           for(Iterator itr=linkedList.iterator();itr.hasNext();)
-           {
-            System.out.println(itr.next());
-           }
-          System.out.println("Size of LinkedList after deletion:"+linkedList.size());
-
+          System.out.println("LinkedList after deletion:");
+       
            linkedList.deleteFirst();
            linkedList.deleteLast();
+          linkedListClient.iterate(linkedList);
+
            System.out.println("Size of LinkedList is:"+linkedList.size());
 
-           for(Iterator itr=linkedList.iterator();itr.hasNext();)
-           {
-            System.out.println(itr.next());
-           }
+          
     }
 }
