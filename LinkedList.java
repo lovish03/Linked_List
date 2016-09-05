d7d8fd00f08071b30fa136c06d698a5f43c29222
Lovish <lovish@hemants-MacBook-Pro.local>
2016-09-05 14:01:41 +0530

    Linked List with Custom Iterator

--- LinkedList.java
+++ LinkedList.java
@@ -1,37 +1,92 @@
-class LinkedList<T> {
+import java.util.*;
+class LinkedList<T> implements Iterable<T>
+{
+    private Node first; 
+    private Node current;
+    private int n;
 
-    private Node<T> first; 
+
+ private class Node  
+ {
+     T data; 
+     Node next; 
+ }
  
-    public LinkedList(){
-           first = null;
+    public LinkedList()
+    {
+      first=null;
+      current=null;
+      n=0;
     }
  
-    public void insert(T data) {
-           Node<T> newNode = new Node<T>(data);  
-           newNode.next = first;   
+    public void insert(T data)
+     {
+      Node newNode = new Node();  
+      newNode.data=data;
+      newNode.next=null;
+      if(first==null)
+      {   
            first = newNode;  
+           current = newNode;
+      }
+      else
+      {
+        current.next = newNode;
+        current = newNode;
+      }
+      n++;
     }
  
-    public Node<T> delete()
+    public int size()
+    {
+      return n;
+    }
+
+    public void deleteFirst()
     {
            if(first==null){                
                   System.out.println("LinkedList doesn't contain any Nodes.");
            }
-           Node<T> tempNode = first; 
+           Node tempNode = first; 
            first = first.next; 
-           return tempNode; 
+           n--;
+
     }
     
-  
-    public void displayLinkedList() {
-           System.out.print("Displaying LinkedList ");
-           Node<T> tempDisplay = first;  
-           while (tempDisplay != null){ 
-                  tempDisplay.displayNode();
-                  tempDisplay = tempDisplay.next; 
+
+    public void deleteLast()
+    {
+           if(first==null){                
+                  System.out.println("LinkedList doesn't contain any Nodes.");
            }
-           System.out.println();
-           
+           Node tempNode = first;  
+           while ((tempNode.next).next != null)
+           {
+                  tempNode = tempNode.next; 
+           }    
+           tempNode.next=null;
+           n--;
+
+    }
+
+    public Iterator<T> iterator() 
+    {
+        return new CustomIterator();
+    }
+
+    private class CustomIterator implements Iterator<T>
+    {
+        private Node current = first;
+        public boolean hasNext()  { return current != null;  
+                           }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            T data = current.data;
+            current = current.next; 
+            return data;
+        }
     }
 
   }
--- LinkedListClient.java
+++ LinkedListClient.java
@@ -1,23 +1,30 @@
-public class LinkedListClient {
-    public static void main(String[] args) {
+import java.util.*;
+public class LinkedListClient 
+{
+    public static void main(String[] args) 
+    {
            LinkedList<Integer> linkedList = new LinkedList<Integer>(); 
-           
+           System.out.println("LinkedList is");
            linkedList.insert(11);
            linkedList.insert(33);
            linkedList.insert(22);
            linkedList.insert(14);
            linkedList.insert(39);
- 
-           linkedList.displayLinkedList(); 
-                        
-           System.out.print("Deleted Nodes: ");
-           Node<Integer> deletedNode = linkedList.delete();  
-           deletedNode.displayNode();                                 
-           deletedNode = linkedList.delete();      
-           deletedNode.displayNode();                                 
-           
-           System.out.println();
-           linkedList.displayLinkedList();   
- 
+           System.out.println("Size of LinkedList is:"+linkedList.size());
+
+           for(Iterator itr=linkedList.iterator();itr.hasNext();)
+           {
+            System.out.println(itr.next());
+           }
+          System.out.println("Size of LinkedList after deletion:"+linkedList.size());
+
+           linkedList.deleteFirst();
+           linkedList.deleteLast();
+           System.out.println("Size of LinkedList is:"+linkedList.size());
+
+           for(Iterator itr=linkedList.iterator();itr.hasNext();)
+           {
+            System.out.println(itr.next());
+           }
     }
-  }
+}
